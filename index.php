5513625110db76521ce4977d3a748e5d98302358
GiandoSaba <giandosaba997@gmail.com>
2022-01-31 15:28:24 +0100

    insert link to snack

--- index.php
+++ index.php
@@ -16,3 +16,22 @@
 // Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo. 
 // slicePar.php
 -->
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blocco PHP Snacks</title>
+</head>
+
+<body>
+    <a href="verifyId.php"> Snack 1</a>
+    <a href="multidimensionalArray.php"> Snack 2</a>
+    <a href="randomNumber.php"> Snack 3</a>
+    <a href="slicePar.php"> Snack 4</a>
+</body>
+
+</html>
