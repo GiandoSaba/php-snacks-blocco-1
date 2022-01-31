83a335f42646333ddccf0a6e83b0384ff5a19076
GiandoSaba <giandosaba997@gmail.com>
2022-01-31 14:41:06 +0100

    init

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php 
+// Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+
+
+// Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 e come valore un array di post associati a quella data. Stampare ogni data con i relativi post.
+// Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
+
+
+
+// Creare un array con 15 numeri casuali, tenendo conto che l’array non dovrà contenere lo stesso numero più di una volta
+
+
+
+// Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi. Ogni punto un nuovo paragrafo.
+?>
